3957a78f7d1db7477aab2188e0b975f1a7ca2429
Dmitriy <dimkkononov@gmail.com>
2025-01-18 16:58:22 +0300

    add task4

---
+++ task4/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+func Worker(id int, data chan string, ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case val, ok := <-data:
+			if !ok {
+				fmt.Println("stopping worker")
+				return
+			}
+
+			fmt.Printf("worker id: %d | recieved data: %s\n", id, val)
+		}
+	}
+}
+
+func main() {
+	// флаг, в котором указывается кол-во воркеров, по умолчанию 3
+	workersCount := flag.Int("workers", 3, "sets number of workers")
+	flag.Parse()
+
+	// канал, в который отправляются данные
+	data := make(chan string)
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT)
+	defer cancel()
+
+	wg := sync.WaitGroup{}
+	for i := 0; i < *workersCount; i++ {
+		wg.Add(1)
+		// запускаем воркер
+		go func() {
+			defer wg.Done()
+			Worker(i+1, data, ctx)
+		}()
+	}
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				// в случае остановки программы закрываем канал
+				close(data)
+				return
+			case data <- time.Now().String():
+				time.Sleep(500 * time.Millisecond)
+			}
+		}
+	}()
+
+	// дожидаемся завершения воркеров
+	wg.Wait()
+
+	fmt.Println("work is finished")
+}
