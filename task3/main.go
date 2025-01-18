3b2fea271626778c967d1d2984a9f69288215bfa
Dmitriy <dimkkononov@gmail.com>
2025-01-18 15:59:59 +0300

    add task 3

---
+++ task3/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func Squares(nums []int) chan int {
+	wg := &sync.WaitGroup{}
+	squaresOut := make(chan int)
+
+	for _, v := range nums {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			squaresOut <- v * v
+		}()
+	}
+
+	go func() {
+		defer close(squaresOut) // закрываем канал
+		wg.Wait()               // дожидаемся завершения горутин
+	}()
+
+	return squaresOut
+}
+
+func SquaresSum(squaresIn chan int) int {
+	sum := 0
+
+	// читаем значения квадратов из канала, пока он не будет закрыт
+	for v := range squaresIn {
+		sum += v
+	}
+
+	return sum
+}
+
+func main() {
+	nums := []int{2, 4, 6, 8, 10}
+	squares := Squares(nums)
+	sum := SquaresSum(squares)
+
+	fmt.Printf("Сумма квадратов: %d\n", sum)
+}
