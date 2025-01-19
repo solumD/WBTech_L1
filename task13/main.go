77e5dd950489ea99920f0edb40fc0eacd51604d8
Dmitriy <dimkkononov@gmail.com>
2025-01-19 19:39:18 +0300

    add task13

---
+++ task13/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+/* Поменять местами два числа без создания временной переменной. */
+
+func main() {
+	a, b := 10, 20
+	fmt.Printf("a: %d | b: %d\n", a, b)
+
+	a, b = b, a
+	fmt.Printf("a: %d | b: %d\n", a, b)
+}
